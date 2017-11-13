5869f171524eb52e4c5ddb0d234fc65059aebcf4
Johnfietsen <lucstefelmanns@live.nl>
2017-11-13 15:01:52 +0100

    Nieuwe stukjes codes voor algorithme en premises voor het programma waarin geopereerd moet worden.

---
+++ Algorithms/test_placement.py
@@ -0,0 +1,11 @@
+# Group:        726567756982776978
+# Assignment:   Amstelhaege
+
+'''
+First algorithm that places houses, used to test the classes of classes.py.
+'''
+
+import classes.py
+import premises.py
+
+grid = Map()
