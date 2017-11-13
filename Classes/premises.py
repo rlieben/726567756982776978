ef1988c8332f8da42817bfd4a52d143aaf8e9366
Johnfietsen <lucstefelmanns@live.nl>
2017-11-13 15:46:06 +0100

    Delete premises

--- Classes/premises.py
+++
@@ -1,8 +0,0 @@
-# Group:        726567756982776978
-# Assignment:   Amstelhaege
-
-WIDTH_MAP = 160
-HEIGHT_MAP = 180
-WATER_PREVALENCE = 0.20
-NR_HOUSES = [20, 40, 60]
-DISTR_HOUSES = [0.60, 0.25, 0.15] # [one_family, bungalow, mansion]
