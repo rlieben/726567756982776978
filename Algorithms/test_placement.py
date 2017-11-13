8bc990fbc60db1db5e41efd3c2afebda0651c7c6
Johnfietsen <lucstefelmanns@live.nl>
2017-11-13 15:38:10 +0100

    No magic numbers

--- Algorithms/test_placement.py
+++ Algorithms/test_placement.py
@@ -5,6 +5,12 @@
 First algorithm that places houses, used to test the classes of classes.py.
 '''
 
+WIDTH_MAP = 160
+HEIGHT_MAP = 180
+WATER_PREVALENCE = 0.20
+NR_HOUSES = [20, 40, 60]
+DISTR_HOUSES = [0.60, 0.25, 0.15] # [one_family, bungalow, mansion]
+
 import classes.py
 import premises.py
 
