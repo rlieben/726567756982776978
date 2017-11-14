5ce366a684fd80b99e3eacc4d63418ad809348b4
TVHTE <31651919+TVHTE@users.noreply.github.com>
2017-11-14 11:46:42 +0100

    Zodat Luc verder kan

--- Scripts/grid.py
+++ Scripts/grid.py
@@ -5,35 +5,25 @@ import matplotlib.pyplot as plot
 import numpy as np
 
 from test_placement import *
+# from classes import *
 
 # make values from -5 to 5, for this
 '''Uiteindelijk moet bij zvals de list worden ingeladen met de waardes die zijn uitgerekend'''
-zvals = np.random.rand(100,100)*10-5
 
-print(zvals)
+# zvals = np.random.rand(100,100)*10-5
+# print(zvals)
 
-<<<<<<< HEAD
-z_vals = ah_map
-
-cell_int = 0
+z_vals = create_test()
+int_grid = [[0 for x in range(HEIGHT_MAP)] for y in range(WIDTH_MAP)]
 
 # change values into integers
 for line in z_vals.grid:
     for cell in line:
         if cell.type == "house":
-            cell_int = 1
-            print(cell_int)
+            int_grid[line][cell] = int(1)
         else:
-            cell_int = 0
+            cell.type = int(0)
 
-print(z_vals)
-=======
-zvals_2 = ah_map
-
-# for line in grid.grid:
-#     if Cell.type == 'House':
-#         print(line)
->>>>>>> c4871f995538e5fc02d3d20aa09812371594398c
 
 # make a color map of fixed colors
 cmap = mpl.colors.ListedColormap(['red','black','white'])
@@ -41,11 +31,7 @@ bounds=[-6,-2,2,6]
 norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
 
 # tell imshow about color map so that only set colors are used
-<<<<<<< HEAD
-img = plot.imshow(cell_int,interpolation='nearest',
-=======
-img = plot.imshow(zvals_2,interpolation='nearest',
->>>>>>> c4871f995538e5fc02d3d20aa09812371594398c
+img = plot.imshow(z_vals.grid,interpolation='nearest',
                     cmap = cmap, norm=norm)
 
 # make a color bar
