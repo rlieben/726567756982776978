7dc7e922ec9fb30f474df5617aa2c84a047e6ac3
Johnfietsen <lucstefelmanns@live.nl>
2017-11-14 15:54:49 +0100

    Check for conflicts

--- Scripts/functions.py
+++ Scripts/functions.py
@@ -22,11 +22,16 @@ def place_one_family(ah_map, loc, house_id):
             ah_map.houses[len(ah_map.houses) - 1].add_structure({'x' : i,
                                                                  'y' : j})
 
-    # changes the type in the grid of the map
+
+    # checks if house can be placed
     for cell in ah_map.houses[len(ah_map.houses) - 1].structure:
 
         if ah_map.grid[cell['x']][cell['y']].type != 'empty':
             return False
+
+
+    # changes the type in the grid of the map
+    for cell in ah_map.houses[len(ah_map.houses) - 1].structure:
 
         ah_map.grid[cell['x']][cell['y']].type = 'one_family'
 
@@ -48,11 +53,15 @@ def place_bungalow(ah_map, loc, house_id):
             ah_map.houses[len(ah_map.houses) - 1].add_structure({'x' : i,
                                                                  'y' : j})
 
-    # changes the type in the grid of the map
+    # checks if house can be placed
     for cell in ah_map.houses[len(ah_map.houses) - 1].structure:
 
         if ah_map.grid[cell['x']][cell['y']].type != 'empty':
             return False
+
+
+    # changes the type in the grid of the map
+    for cell in ah_map.houses[len(ah_map.houses) - 1].structure:
 
         ah_map.grid[cell['x']][cell['y']].type = 'bungalow'
 
@@ -74,12 +83,16 @@ def place_mansion(ah_map, loc, house_id):
             ah_map.houses[len(ah_map.houses) - 1].add_structure({'x' : i,
                                                                  'y' : j})
 
-    # changes the type in the grid of the map
+    # checks if house can be placed
     for cell in ah_map.houses[len(ah_map.houses) - 1].structure:
 
         if ah_map.grid[cell['x']][cell['y']].type != 'empty':
             return False
-        
+
+
+    # changes the type in the grid of the map
+    for cell in ah_map.houses[len(ah_map.houses) - 1].structure:
+
         ah_map.grid[cell['x']][cell['y']].type = 'mansion'
 
     return ah_map
