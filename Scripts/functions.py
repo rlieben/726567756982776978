cf4f76f1b145e683db3a228475301be7ff9b5109
TVHTE <31651919+TVHTE@users.noreply.github.com>
2017-11-21 14:02:25 +0100

    -

--- Scripts/functions.py
+++ Scripts/functions.py
@@ -17,10 +17,6 @@ All basic functions to operate on the map.
 def place_house(ah_map, loc, house_id, type_charac):
     '''Places a one family house on location on the map, with a given id.'''
 
-    # adds class to list of houses in the map
-    # for i in range(house_id - 1):
-    #     free_space = loc['x'][i] - loc['x'][i + 1]
-    #     if free_space > ONE_FAM['min_free']
     ah_map.houses.append(House(house_id, type_charac, loc))
 
     return ah_map
