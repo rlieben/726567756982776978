4890af462f1a606839f7afac1b1004f76c46d466
Raoul Lieben <rlieben@live.nl>
2017-11-21 13:42:49 +0100

    calc freespace update

--- Scripts/calc_freespace.py
+++ Scripts/calc_freespace.py
@@ -10,8 +10,7 @@ def calc_freespace(newhouse, ah_map):
 	x_newhouse = newhouse.loc['x']
 	y_newhouse = newhouse.loc['y']
 
-	# set freespace variable
-	# calculate x and y difference new and curr
+	# calculate x and y difference new and curr and calc freespace variable
 	diff_housescurr[0] = ah_map.houses[house].loc['x'] - x_newhouse
 	diff_housescurr[1] = ah_map.houses[house].loc['y'] - y_newhouse
 	freespace = sqrt((diff_housescurr[0] ^ 2) + (diff_housescurr[1] ^ 2))
@@ -23,16 +22,11 @@ def calc_freespace(newhouse, ah_map):
 		diff_housescurr[0] = ah_map.houses[house].loc['x'] - x_newhouse
 		diff_housescurr[1] = ah_map.houses[house].loc['y'] - y_newhouse
 
-		# calculate x and y difference new and next
-		diff_housesnext[0] = ah_map.houses[house + 1].loc['x'] - x_newhouse
-		diff_housesnext[1] = ah_map.houses[house + 1].loc['y'] - y_newhouse
-
 		# calculates distance between new and current
 		distancecurr = sqrt((diff_housescurr[0] ^ 2) + (diff_housescurr[1] ^ 2))
-		distancenext = sqrt((diff_housesnext[0] ^ 2) + (diff_housesnext[1] ^ 2))
 
 		# update if distance from current is greater than next
-		if freespace > distancenext:
+		if freespace >= distancenext:
 
 			freespace = distancenext
 
