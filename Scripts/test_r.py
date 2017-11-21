39f7d607e7103ccc4bfc1454403ae790846bf5f4
Johnfietsen <lucstefelmanns@live.nl>
2017-11-21 13:25:29 +0100

    Infrastructuur verbeteren.

---
+++ Scripts/test_r.py
@@ -0,0 +1,10 @@
+# Study:        Minor Programming, University of Amsterdam
+# Course:       Heuristics
+# Assignment:   Amstelhaege
+# Group:        726567756982776978
+# Members:      Toon van Holte, Raoul Lieben, Luc Stefelmanns
+
+
+'''
+Test script for Raoul.
+'''
