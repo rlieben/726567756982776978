f7b6a69a405f7965bb4665051bc1f089bcbb540d
TVHTE <31651919+TVHTE@users.noreply.github.com>
2017-11-23 12:34:25 +0100

    hill

--- Scripts/algorithms.py
+++ Scripts/algorithms.py
@@ -36,23 +36,99 @@ def random_generator():
             allowed = place_house(ah_map, loc, house_id, ONE_FAM)
 
     for i in range(BU):
-        rand_y_loc = random.uniform(0,MAP['height'] - (BUNGALOW['height']/2))
-        rand_x_loc = random.uniform(0,MAP['width'] - (BUNGALOW['width']/2))
 
-        loc = {'x':rand_x_loc,'y':rand_y_loc}
+        allowed = False
 
-        house_id = i
+        while allowed == False:
+            rand_y_loc = random.uniform(0,MAP['height'] - (BUNGALOW['height']/2))
+            rand_x_loc = random.uniform(0,MAP['width'] - (BUNGALOW['width']/2))
 
-        place_house(ah_map, loc, house_id, BUNGALOW)
+            loc = {'x':rand_x_loc,'y':rand_y_loc}
+
+            house_id = i
+
+            allowed = place_house(ah_map, loc, house_id, BUNGALOW)
 
     for i in range(MA):
-        rand_y_loc = random.uniform(0,MAP['height'] - (MANSION['height']/2))
-        rand_x_loc = random.uniform(0,MAP['width'] - (MANSION['width']/2))
 
-        loc = {'x':rand_x_loc,'y':rand_y_loc}
+        allowed = False
 
-        house_id = i
+        while allowed == False:
+            rand_y_loc = random.uniform(0,MAP['height'] - (MANSION['height']/2))
+            rand_x_loc = random.uniform(0,MAP['width'] - (MANSION['width']/2))
 
-        place_house(ah_map, loc, house_id, MANSION)
+            loc = {'x':rand_x_loc,'y':rand_y_loc}
+
+            house_id = i
+
+            allowed = place_house(ah_map, loc, house_id, MANSION)
 
     return ah_map
+
+# input is empty map, output is value calculated
+def hill_climber(ah_map):
+    ah_map = Map(MAP['width'], MAP['height'])
+
+    OF = int(MAP['nr_houses'][0] * MAP['distr_houses'][0])
+    BU = int(MAP['nr_houses'][0] * MAP['distr_houses'][1])
+    MA = int(MAP['nr_houses'][0] * MAP['distr_houses'][2])
+
+    for i in range(OF):
+
+        allowed = False
+
+        while allowed == False:
+            rand_y_loc = random.uniform(0,MAP['height'] - (ONE_FAM['height']/2))
+            rand_x_loc = random.uniform(0,MAP['width'] - (ONE_FAM['width']/2))
+
+            loc = {'x':rand_x_loc,'y':rand_y_loc}
+
+            house_id = i
+
+            allowed = place_house(ah_map, loc, house_id, ONE_FAM)
+
+    for i in range(BU):
+
+        allowed = False
+
+        while allowed == False:
+            rand_y_loc = random.uniform(0,MAP['height'] - (BUNGALOW['height']/2))
+            rand_x_loc = random.uniform(0,MAP['width'] - (BUNGALOW['width']/2))
+
+            loc = {'x':rand_x_loc,'y':rand_y_loc}
+
+            house_id = i
+
+            allowed = place_house(ah_map, loc, house_id, BUNGALOW)
+
+    for i in range(MA):
+
+        allowed = False
+
+        while allowed == False:
+            rand_y_loc = random.uniform(0,MAP['height'] - (MANSION['height']/2))
+            rand_x_loc = random.uniform(0,MAP['width'] - (MANSION['width']/2))
+
+            loc = {'x':rand_x_loc,'y':rand_y_loc}
+
+            house_id = i
+
+            allowed = place_house(ah_map, loc, house_id, MANSION)
+
+    summy = 0
+    maximum = 0
+
+    for house in ah_map.houses:
+        ah_map.calc_freespace(house)
+        house.calc_value()
+        # print(house.charac['type'])
+        # print(house.value)
+        if house.value < 0:
+            print(house.value)
+        summy += house.value
+
+    if summy > maximum:
+        maximum = summy
+        best_map = ah_map
+
+    return maximum, best_map
