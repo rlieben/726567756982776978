edd972e1e90e61661734790e8da3919170c7c7dd
Raoul Lieben <rlieben@live.nl>
2017-11-24 15:31:24 +0100

    calc freespace on map

--- Scripts/functions.py
+++ Scripts/functions.py
@@ -28,3 +28,46 @@ def place_house(ah_map, loc, house_id, type_charac):
         else:
             ah_map.houses.append(new_house)
         # return ah_map
+
+def calc_freespace_on_map(ah_map, new_house):
+    '''Calculating most freespace on map and receives map and movable house as input'''
+
+    # initiate possible freespace variable
+    poss_freespace = 0
+
+    # initiate x and y variable for optimization
+    best_x = new_house.location['x']
+    best_y =  new_house.location['y']
+
+    # iterate over map width
+    for i in range(len(ah_map.charac['width'])):
+
+        # iterate over map height
+        for j in range(len(ah_map.charac['height'])):
+
+            # set x and y location of new house
+            new_house.location['x'] = i
+            new_house.location['y'] = j
+
+            # store the possible freespace of new location
+            tmp = ah_map.calc_freespace(new_house)
+
+            # if new freespace is greater then update
+            if (tmp > poss_freespace):
+
+                # update new poss freespace
+                poss_freespace = tmp
+
+                # update location of poss freespace
+                best_x = i
+                best_y = j
+
+            # increment j to skip through map
+            j += 5
+
+        # increment i to skip through map
+        i += 5
+
+
+
+
