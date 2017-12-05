3e67050898dddee391aa4a4449417b693718134d
Johnfietsen <lucstefelmanns@live.nl>
2017-12-05 12:27:40 +0100

    Fixes.

--- Running_Scripts/tactical_hillclimber.py
+++ Running_Scripts/tactical_hillclimber.py
@@ -1,22 +1,5 @@
 
-import sys
-list_dir = sys.path[0].split('/')
-string = ''
-for i in range(len(list_dir) - 1):
-    string += list_dir[i]
-    string += '/'
-
-sys.path.insert(0, string)
-
-from Classes.house import *
-from Classes.map import *
-from Classes.water import *
-from Characteristics.Amstelhaege import *
-from Algorithms.helpers import *
-from Algorithms.best_of_random import *
-from Algorithms.hillclimber import *
-from Algorithms.tactical_hillclimber import *
-from Visuals.scatterplot import *
+from __import__ import *
 
 best_hill = tactical_hillclimber(MAP_20, 10, 10, 1)
 print('tactical:', best_hill.score)
