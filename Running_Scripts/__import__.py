5c1abce75a5ca165a5c8c38c6947201a6389abc4
Raoul Lieben <rlieben@live.nl>
2017-12-05 12:42:02 +0100

    import adjust

--- Running_Scripts/__import__.py
+++ Running_Scripts/__import__.py
@@ -20,3 +20,7 @@ from Algorithms.hillclimber import *
 from Algorithms.tactical_hillclimber import *
 from Algorithms.depthfirst import *
 from Visuals.scatterplot import *
+
+import numpy
+import random
+import copy
