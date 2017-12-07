1cd53d1f0b07aa8c389e9116101d915405d4591b
TVHTE <31651919+TVHTE@users.noreply.github.com>
2017-12-07 16:05:31 +0100

    met water

--- Running_Scripts/test.py
+++ Running_Scripts/test.py
@@ -2,11 +2,7 @@
 from __import__ import *
 
 ah_map = Map(MAP_20)
-ah_map.place_water({'x' : 50, 'y' : 50}, 1)
-print(ah_map.water[0].corners)
-
-scatterplot(ah_map, 'test')
-
+ah_map.place_water({'x': random.randint(0,160), 'y': random.randint(0,160)}, 1)
 # allowed = ah_map.place_house({'x' : 50, 'y' : 50}, 1, ah_map.types[0])
 # print(allowed)
 # allowed = ah_map.place_house({'x' : 50, 'y' : 40}, 2, ah_map.types[0])
@@ -16,5 +12,7 @@ scatterplot(ah_map, 'test')
 # 	print(house.freespace)
 
 # ah_map = random_generator(MAP_20)
-
+#
 # fireworks(MAP_20)
+
+scatterplot(ah_map, 'test')
