c2fc57a10042a00dbc7df59e04ee613cbf9ef79a
TVHTE <31651919+TVHTE@users.noreply.github.com>
2017-12-18 14:00:47 +0100

    runme update

--- RUNME.py
+++ RUNME.py
@@ -1,147 +1,147 @@
+# Study:        Minor Programming, University of Amsterdam
+# Course:       Heuristics
+# Assignment:   Amstelhaege
+# Group:        726567756982776978
+# Members:      Toon van Holthe, Raoul Lieben, Luc Stefelmanns
 
 import sys
 
 from __import__ import *
 
-# while True:
-#     try:
-#         command = int(input("Please enter algorithm: "))
-#
-#     except ValueError:
-#         print("Sorry, I didn't understand that.")
-#         continue
-#     else:
-#         break
-#
-# if  command > 5:
-#     print('Give valid algorithm')
-# else:
-# 	if command == 1:
-# 		best_random_map = best_of_random(MAP_20, 19)
-#
-# 		coloured_map(best_random_map['best_map'], 'run_me', 'best')
-# 		plot_data([best_random_map['data']], 'run_me', 'plot')
-# 		save_results(best_random_map['data'], 'run_me', 'data')
-#
-# 	elif command == 2:
-# 		best_greedy = greedy(20, MAP_20)
-#
-# 		coloured_map(best_greedy['best_map'], 'run_me', 'best')
-# 		plot_data([best_greedy['data']], 'run_me', 'plot')
-# 		save_results(best_greedy['data'], 'run_me', 'data')
-#
-# 	elif command == 3:
-# 		hillclimber_map = hillclimber(MAP_20, 1, 20, 1, 0)
-#
-# 		coloured_map(hillclimber_map['best_map'], 'run_me', 'best')
-# 		plot_data([hillclimber_map['data']], 'run_me', 'plot')
-# 		save_results(hillclimber_map['data'], 'run_me', 'data')
-#
-# 	elif command == 4:
-# 		force_move_map = force_move(random_map, 20)
-#
-# 		coloured_map(force_move_map['best_map'], 'run_me', 'best')
-# 		plot_data([force_move_map['data']], 'run_me', 'plot')
-# 		save_results(force_move_map['data'], 'run_me', 'data')
-#
-# 	elif command == 5:
-# 		sa_hillclimber_map = sa_hillclimber(MAP_20, 10, 1, 0, 0.01, 4)
-#
-# 		coloured_map(sa_hillclimber_map['best_map'], 'run_me', 'best')
-# 		plot_data([sa_hillclimber_map['data']], 'run_me', 'plot')
-# 		save_results(sa_hillclimber_map['data'], 'run_me', 'data')
+def run_me():
+    '''Runs the case'''
+    
+    # get number of houses
+    map_input = int(input('Enter number of houses (20, 40 or 60): '))
+
+    # check if correct input is given
+    allowed = False
+
+    while allowed == False:
+        if map_input == 20:
+            allowed = True
+        elif map_input == 40:
+            allowed = True
+        elif map_input == 60:
+            allowed = True
+        else:
+            map_input = int(input('Invalid number of houses, try again: '))
+            allowed = False
+
+    # get the algorithm
+    alg = input('Enter algorithm of your choice: ')
+
+    # get number of iterations
+    num_it = int(input('Enter number of iterations: '))
+
+    #  check if correct input is given
+    allowed = False
+
+    while allowed == False:
+        if num_it < 0:
+            num_it = int(input('No negative integers, try again: '))
+        else:
+            allowed = True
 
 
-map_input = int(input('Enter number of houses (20,40 or 60): '))
+    def main(alg, num_it, map_specs):
 
-allowed = False
+        '''Runs the algorithm of choice.
 
-while allowed == False:
-    if map_input == 20:
-        map_specs = MAP_20
-        allowed = True
-    elif map_input == 40:
-        map_specs = MAP_40
-        allowed = True
-    elif map_input == 60:
-        map_specs = MAP_60
-        allowed = True
-    else:
-        map_input = int(input('Invalid number of houses, try again: '))
-        allowed = False
+        Input arguments:
+        alg -- algorithm of choice
+        num_it -- number of iterations
+        map_specs -- type specifications of the case, from file in folder specifications
+        '''
 
-arg = input('Enter algorithm of your choice: ')
-num_it = int(input('Enter number of iterations: '))
+        # set map_specs to right specifications
+        if map_specs == 20:
+            map_specs = MAP_20
+        elif map_specs == 40:
+            map_specs = MAP_40
+        elif map_specs == 60:
+            map_specs = MAP_60
 
-def main(arg, num_it, map_specs):
+        if alg == 'hillclimber':
+            print('Running hillclimber...')
 
-    if arg == 'hillclimber':
-        print('Running hillclimber...')
+            hillclimber_map = hillclimber(map_specs, 1, num_it, 1, 0, False)
 
-        hillclimber_map = hillclimber(map_specs, 1, num_it, 1, 0)
+            coloured_map(hillclimber_map['best_map'], 'run_me', 'best')
+            plot_data([hillclimber_map['data']], 'run_me', 'plot')
+            save_results(hillclimber_map['data'], 'run_me', 'data')
 
-        coloured_map(hillclimber_map['best_map'], 'run_me', 'best')
-        plot_data([hillclimber_map['data']], 'run_me', 'plot')
-        save_results(hillclimber_map['data'], 'run_me', 'data')
+        elif alg == 'best of random':
+            print('Running best of random...')
 
-    elif arg == 'best of random':
-        print('Running best of random...')
+            best_random_map = best_of_random(map_specs, num_it)
 
-        best_random_map = best_of_random(map_specs, num_it)
-
-        coloured_map(best_random_map['best_map'], 'run_me', 'best')
-        plot_data([best_random_map['data']], 'run_me', 'plot')
-        save_results(best_random_map['data'], 'run_me', 'data')
+            coloured_map(best_random_map['best_map'], 'run_me', 'best')
+            plot_data([best_random_map['data']], 'run_me', 'plot')
+            save_results(best_random_map['data'], 'run_me', 'data')
 
 
-    elif arg == 'force move':
-        print('Running force move...')
+        elif alg == 'force move':
+            print('Running force move...')
 
-        force_move_map = force_move(random_map, num_it)
+            force_move_map = force_move(random_map, num_it)
 
-        coloured_map(force_move_map['best_map'], 'run_me', 'best')
-        plot_data([force_move_map['data']], 'run_me', 'plot')
-        save_results(force_move_map['data'], 'run_me', 'data')
+            coloured_map(force_move_map['best_map'], 'run_me', 'best')
+            plot_data([force_move_map['data']], 'run_me', 'plot')
+            save_results(force_move_map['data'], 'run_me', 'data')
 
-    elif arg == 'greedy':
-        print('Running greedy...')
+        elif alg == 'greedy':
+            print('Running greedy...')
 
-        best_greedy = greedy(num_it, map_specs)
+            best_greedy = greedy(num_it, map_specs)
 
-        coloured_map(best_greedy['best_map'], 'run_me', 'best')
-        plot_data([best_greedy['data']], 'run_me', 'plot')
-        save_results(best_greedy['data'], 'run_me', 'data')
+            coloured_map(best_greedy['best_map'], 'run_me', 'best')
+            plot_data([best_greedy['data']], 'run_me', 'plot')
+            save_results(best_greedy['data'], 'run_me', 'data')
 
 
-    elif arg == 'simmulated annealing':
-        print('Running simulated annealing...')
+        elif alg == 'simmulated annealing':
+            print('Running simulated annealing...')
 
-        sa_hillclimber_map = sa_hillclimber(map_specs, num_it, 1, 0, 0.01, 4)
+            sa_hillclimber_map = sa_hillclimber(map_specs, num_it, 1, 0, 0.01, 4)
 
-        coloured_map(sa_hillclimber_map['best_map'], 'run_me', 'best')
-        plot_data([sa_hillclimber_map['data']], 'run_me', 'plot')
-        save_results(sa_hillclimber_map['data'], 'run_me', 'data')
+            coloured_map(sa_hillclimber_map['best_map'], 'run_me', 'best')
+            plot_data([sa_hillclimber_map['data']], 'run_me', 'plot')
+            save_results(sa_hillclimber_map['data'], 'run_me', 'data')
 
-    else:
-        arg = input('Inalid input argument, try again.\n Enter algorithm of your choice: ')
+        else:
+            alg = input('Inalid algorithm, try again.\nEnter algorithm of your choice: ')
 
-        allowed = False
+            allowed = False
 
-        while allowed == False:
-            if map_input == 20:
-                map_specs = MAP_20
-                allowed = True
-            elif map_input == 40:
-                map_specs = MAP_40
-                allowed = True
-            elif map_input == 60:
-                map_specs = MAP_60
-                allowed = True
-            else:
-                map_input = int(input('Invalid number of houses, try again: '))
-                allowed = False
+            map_input = int(input('Enter number of houses (20, 40 or 60): '))
 
-        num_it = int(input('Number of iterations: '))
-        main(arg, num_it, map_input)
+            while allowed == False:
+                if map_input == 20:
+                    map_specs = MAP_20
+                    allowed = True
+                elif map_input == 40:
+                    map_specs = MAP_40
+                    allowed = True
+                elif map_input == 60:
+                    map_specs = MAP_60
+                    allowed = True
+                else:
+                    map_input = int(input('Invalid number of houses, try again: '))
+                    allowed = False
 
-main(arg, num_it, map_input)
+            num_it = int(input('Number of iterations: '))
+
+            allowed = False
+
+            while allowed == False:
+                if num_it < 0:
+                    num_it = int(input('No negative integers, try again: '))
+                else:
+                    allowed = True
+
+            main(alg, num_it, map_input)
+
+    main(alg, num_it, map_input)
+
+run_me()
